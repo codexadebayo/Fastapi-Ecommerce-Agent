535f8dac346db84034a57f6839bdf9a450b32273
Codexadebayo <whitepaperade@gmail.com>
2025-05-10 09:33:13 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+def create_app():
+    """Create a FastAPI app."""
+    return FastAPI()
+
+
+app = create_app()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
