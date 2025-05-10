e148ad687e06964b82badbe8eae32ddaf240d242
Codexadebayo <whitepaperade@gmail.com>
2025-05-10 14:51:13 +0100

    fix: alembi config for user model

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import pool
 
 from alembic import context
 from config.db import Base
+from models.user import User  # Import the User model
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
